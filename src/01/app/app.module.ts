13115de87f4dcf176d6c187eeadc5c11016ebd27
vellaichamy <uvellaichamy@gmail.com>
2021-08-07 23:04:32 +0530

    6 files modified, 2 added

--- src/01/app/app.module.ts
+++ src/01/app/app.module.ts
@@ -2,22 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+
 import { AppComponent } from './app.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
-import { ServersComponent } from './servers/servers.component';
-import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    WarningAlertComponent,
-    SuccessAlertComponent,
-    ServersComponent,
-    ServerComponent
+    AppComponent
   ],
-  imports: [BrowserModule, FormsModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- src/01/app/servers/servers.component.ts
+++ src/01/app/servers/servers.component.ts
@@ -4,14 +4,31 @@ import { Component, OnInit } from '@angular/core';
   // selector: '[app-servers]',
   // selector: '.app-servers',
   selector: 'app-servers',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>
-  `,
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-  constructor() {}
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'Testserver';
 
-  ngOnInit() {}
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
