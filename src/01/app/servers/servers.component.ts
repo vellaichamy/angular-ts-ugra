9a0c5b09b7d20dda904a49accdef7bad438f8cb8
vellaichamy <uvellaichamy@gmail.com>
2021-08-07 23:40:36 +0530

    8 files modified

--- src/01/app/servers/servers.component.ts
+++ src/01/app/servers/servers.component.ts
@@ -14,6 +14,8 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = 'Testserver';
+  serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
 
   constructor() {
     setTimeout(() => {
@@ -25,6 +27,8 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
