b3637e40ad5e5e05fba0c5ce448d9c6f2141d644
vellaichamy <uvellaichamy@gmail.com>
2021-08-08 23:20:27 +0530

    4 files modified

--- src/10/app/app.component.ts
+++ src/10/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 
-import { UserService } from './users.service';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [UserService]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
