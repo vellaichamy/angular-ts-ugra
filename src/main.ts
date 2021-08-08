53123d83c12e675c947658b97d648bdc303c5cc6
vellaichamy <uvellaichamy@gmail.com>
2021-08-08 16:18:36 +0530

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,7 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './05/app/app.module';
-1;
-1;
+
 // import { AppModule } from './02/app/app.module';
 // import { AppModule } from './01/app/app.module';
 // import { AppModule } from './app/app.module';
