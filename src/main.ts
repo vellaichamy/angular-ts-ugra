e683ad72374579cbb6fbf01bdad7fbf45d1e1225
vellaichamy <uvellaichamy@gmail.com>
2021-08-08 12:52:11 +0530

    1 file modified, 9 added

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import './polyfills';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './02/app/app.module';
+import { AppModule } from './05/app/app.module';
+1;
+1;
+// import { AppModule } from './02/app/app.module';
 // import { AppModule } from './01/app/app.module';
 // import { AppModule } from './app/app.module';
 
